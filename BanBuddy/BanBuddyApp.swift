e49c0d4c795c56c60ab12564e27838d00acb78ff
NovaNecros <uchihaespurr@gmail.com>
2025-07-02 12:13:32 -0600

    Initial Commit

---
+++ BanBuddy/BanBuddyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BanBuddyApp.swift
+//  BanBuddy
+//
+//  Created by Desarrollo Finantech on 02/07/25.
+//
+
+import SwiftUI
+
+@main
+struct BanBuddyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BanBuddyTests/BanBuddyTests.swift
@@ -0,0 +1,17 @@
+//
+//  BanBuddyTests.swift
+//  BanBuddyTests
+//
+//  Created by Desarrollo Finantech on 02/07/25.
+//
+
+import Testing
+@testable import BanBuddy
+
+struct BanBuddyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

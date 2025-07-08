883ec0fbcd856d66f9726931f39f65bea415a245
NovaNecros <uchihaespurr@gmail.com>
2025-07-08 14:26:11 -0600

    Creé la pantalla para consultar el historial de movimientos. Está vacía por ahora.

---
+++ BanBuddy/Movimientos.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+struct Movimientos : View
+{
+    var body : some View
+    {
+        NavigationStack()
+        {
+        
+            VStack(alignment: .center, spacing: 0)
+            {
+                
+            }
+            .navigationBarBackButtonHidden(true)
+            .padding()
+        }
+    }
+}
+
+#Preview
+{
+    Movimientos()
+}
